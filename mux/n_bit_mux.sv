99212b2b22fd75a46a2aaa43b655fdda321d4e8c
Wissem <w.kharrat@stud.uni-hannover.de>
2025-09-19 13:12:47 +0200

    implement n_bit_mux

---
+++ mux/n_bit_mux.sv
@@ -0,0 +1,14 @@
+module n_bit_mux
+    (
+        input logic a , b , c , d 
+        input logic sel[1:0],
+        output logic f
+    ) ;
+
+    logic f1, f2;
+
+    mux mux1(.a(a), .b(b), .sel(sel[0]), .f(f1));
+    mux mux2(.a(c), .b(d), .sel(sel[0]), .f(f2));
+
+    mux mux3(.a(f1), .b(f2), .sel(sel[1]), .f(f));
+endmodule
