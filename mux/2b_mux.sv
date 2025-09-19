e637cda42434094db4c564c2157fd05eba38134b
Wissem <w.kharrat@stud.uni-hannover.de>
2025-09-19 13:02:53 +0200

    fix typo

--- mux/2b_mux.sv
+++ mux/2b_mux.sv
@@ -6,5 +6,5 @@ module 2b_mux
     ) ;
 
     mux mux1(.a(a[0]), .b(b[0]), .sel(sel), .f(f[0]));
-    mux mux1(.a(a[1]), .b(b[1]), .sel(sel), .f(f[1]));
+    mux mux2(.a(a[1]), .b(b[1]), .sel(sel), .f(f[1]));
 endmodule
