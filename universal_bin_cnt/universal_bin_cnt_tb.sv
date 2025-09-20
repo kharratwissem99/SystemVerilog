742ceaa4d104c616527cb1bd9468065b3d7743bb
Wissem <kharrat.wissem99@gmail.com>
2025-09-21 00:02:14 +0200

    fix synthacs errors

--- universal_bin_cnt/universal_bin_cnt_tb.sv
+++ universal_bin_cnt/universal_bin_cnt_tb.sv
@@ -7,13 +7,13 @@ module universal_bin_cnt_tb;
     //   reg a, b;
     //   wire q0, q1;
     localparam N = 5;
-    input logic clk , rst , syn_clr , load, en, up,
-    input logic [N-1:0] d,
-    output logic [N-1:0] q
+    logic clk , rst , syn_clr , load, en, up;
+    logic [N-1:0] d;
+    logic [N-1:0] q;
 
   // DUT (Device Under Test)
   // Using your code wrapped in a module
-  universal_bin_cnt dut (N)(
+  universal_bin_cnt #(N) dut(
     .clk(clk) , .rst(rst) , .syn_clr(syn_clr) , .load(load), .en(en), .up(up), .d(d), .q(q)
   );
 
@@ -24,19 +24,19 @@ module universal_bin_cnt_tb;
   initial begin
     // Initialize signals
     clk = 0; rst = 1; // move to always block
-    syn_clr = 0; load = 0, en = 1, up = 1, d = '0
+    syn_clr = 0; load = 0; en = 1; up = 1; d = '0;
 
     // Monitor outputs
-    $monitor("t=%0t clk=%b a=%b b=%b | q0=%b q1=%b", 
-              $time, clk, a, b, q0, q1);
+    $monitor("t=%0t clk=%b rst=%b load=%b up=%b en=%b d=%b | q=%b", 
+              $time, clk, rst, load, up, en, d, q);
 
     // Change inputs over time
     #1 rst = 0;
     // let the counter count up
 
-    #50 load = 1; up = 0; en = 1; d = '20; // load 20 and let him count down
+    #50 load = 1; up = 0; en = 1; d = 'd20; // load 20 and let him count down
 
-    #50 load = 0; up = 0; en = 0; d = '20; // pause
+    #50 load = 0; up = 0; en = 0; d = 'd20; // pause
 
     // #5  a=1; b=0; clk=1;   // q0 latch transparent -> q0 = a&b = 0
     // #5  a=1; b=1;          // q0 updates -> q0 = 1
